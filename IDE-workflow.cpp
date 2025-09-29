33ac7c8679f5fd279d3dbb449c11f4acff454f7e
Noah <noahkc@hotmail.com>
2025-09-28 22:05:05 -0400

    Add project files.

---
+++ IDE-workflow.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char first, middle, last;
+    first = 'N';
+    middle = 'K';
+    last = 'C';
+    cout << first << middle << last << endl;
+
+    return 0;
+}
